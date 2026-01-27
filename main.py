4ce72d5a7ff33a992ce7d372861e3e658ee543ab
FrosTether <voluntaryistj@gmail.com>
2026-01-27 16:03:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import kivy as fivy  # Renaming Kivy to Fivy
-from fivy.app import App
-from fivy.uix.label import Label
-
-class FivyApp(App):
-    def build(self):
-        return Label(text='Fivy (Frostit) is Running x25')
-
-if __name__ == '__main__':
-    FivyApp().run()
 import requests
 from requests.auth import HTTPDigestAuth
 import time
